ad7bf6be270c4e10d0b0f0a2b47bb65ece694a22
kanan-mahudhia <kanan.mahudhia46@gmail.com>
2021-05-26 18:01:03 +0530

    Update production.js

--- config/production.js
+++ config/production.js
@@ -7,11 +7,11 @@ module.exports = {
 
   auth: {
     jwt: {
-      secret: '5edb26e5-ec21-4150-9fde-57735eabba99',
+      secret: 'jwt-secret',
       expiresIn:  '24h'
     },
     resetPassword: {
-      secret: '27e048c1-5575-4807-873e-5b3775419286',
+      secret: 'resetpasswordSecret',
     },
   },
 
